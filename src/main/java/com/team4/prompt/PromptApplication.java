8bea87e3ea1f0fa5e331cc3ae4be1e0d55a282dc
JEONG-JIWHAN <jjw981210@gmail.com>
2023-05-03 00:34:45 +0900

    chore: project setting

---
+++ src/main/java/com/team4/prompt/PromptApplication.java
@@ -0,0 +1,13 @@
+package com.team4.prompt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromptApplication.class, args);
+	}
+
+}
