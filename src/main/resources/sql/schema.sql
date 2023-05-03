33eb292689d2f7b586f3ce81d71d3501e19b61e5
JEONG-JIWHAN <jjw981210@gmail.com>
2023-05-03 18:05:02 +0900

    feat: schema.sql 추가

---
+++ src/main/resources/sql/schema.sql
@@ -0,0 +1,72 @@
+
+CREATE TABLE employee
+(
+    id int NOT NULL AUTO_INCREMENT ,
+    user_id varchar(20) NOT NULL ,
+    password varchar(80) NOT NULL ,
+    employee_no varchar(10) NOT NULL ,
+    name varchar(10) NOT NULL ,
+    email varchar(50) NOT NULL ,
+    registration_number varchar(20) NOT NULL ,
+    phone_number varchar(20) NOT NULL ,
+    education varchar(20) NOT NULL ,
+    experience_year int NOT NULL ,
+    entering_date datetime NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    approved boolean NOT NULL DEFAULT false,
+    skill varchar(255) DEFAULT NULL ,
+    c_rank varchar(20) DEFAULT NULL ,
+    position varchar(20) DEFAULT NULL ,
+    role varchar(10) DEFAULT NULL ,
+    deleted boolean NOT NULL DEFAULT FALSE,
+    PRIMARY KEY (id),
+    CONSTRAINT unq_user_id UNIQUE (user_id),
+    CONSTRAINT unq_employee_no UNIQUE (employee_no),
+    CONSTRAINT unq_registration_number UNIQUE (registration_number),
+    CONSTRAINT unq_email UNIQUE (email)
+);
+
+CREATE TABLE project
+(
+    id int NOT NULL AUTO_INCREMENT ,
+    project_no varchar(10) NOT NULL ,
+    name varchar(30) NOT NULL ,
+    create_date datetime NOT NULL DEFAULT CURRENT_TIMESTAMP() ,
+    start_date datetime DEFAULT NULL ,
+    end_date datetime DEFAULT NULL ,
+    client varchar(30) NOT NULL ,
+    budget int NOT NULL DEFAULT 0,
+    state int NOT NULL DEFAULT 0,
+    description varchar(50) DEFAULT NULL,
+    deleted boolean NOT NULL DEFAULT FALSE,
+    PRIMARY KEY (id),
+    CONSTRAINT unq_project_no UNIQUE (project_no)
+);
+
+CREATE TABLE manpower
+(
+    id int NOT NULL AUTO_INCREMENT ,
+    employee_id int NOT NULL ,
+    project_id int NOT NULL ,
+    task varchar(20) NOT NULL ,
+    start_date datetime NOT NULL,
+    end_date datetime NOT NULL ,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_manpower_to_employee FOREIGN KEY (employee_id) REFERENCES employee (id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+    CONSTRAINT fk_manpower_to_project FOREIGN KEY (project_id) REFERENCES project (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+CREATE TABLE evaluation
+(
+    id int NOT NULL AUTO_INCREMENT ,
+    project_id int NOT NULL ,
+    evaluated_id int NOT NULL ,
+    rate int NOT NULL ,
+    contents varchar(80) DEFAULT NULL,
+    item varchar(30) NOT NULL ,
+    type varchar(20) NOT NULL ,
+    end_date datetime DEFAULT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_evaluation_to_manpower FOREIGN KEY (evaluated_id) REFERENCES manpower (id) ON DELETE RESTRICT ON UPDATE RESTRICT ,
+    CONSTRAINT fk_evaluation_to_project FOREIGN KEY (project_id) REFERENCES manpower (id) ON DELETE RESTRICT ON UPDATE RESTRICT
+);
