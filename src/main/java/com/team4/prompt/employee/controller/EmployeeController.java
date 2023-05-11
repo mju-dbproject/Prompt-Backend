fe7986c92942b3c99a1a585d7b52126f0a24d06d
stophwan <jjw981210@gmail.com>
2023-05-11 18:03:10 +0900

    feat: 회원가입 기능 구현

---
+++ src/main/java/com/team4/prompt/employee/controller/EmployeeController.java
@@ -0,0 +1,21 @@
+package com.team4.prompt.employee.controller;
+
+import com.team4.prompt.employee.controller.dto.EmployeeCreateRequest;
+import com.team4.prompt.employee.service.EmployeeService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/employee")
+@RequiredArgsConstructor
+public class EmployeeController {
+    private final EmployeeService employeeService;
+    @PostMapping("/join")
+    public void join(@RequestBody @Valid EmployeeCreateRequest employeeCreateRequest) {
+        employeeService.join(employeeCreateRequest);
+    }
+}
