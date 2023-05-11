3cc3c01acabff07817be2e95ecf27100eccde397
stophwan <jjw981210@gmail.com>
2023-05-11 18:02:53 +0900

    feat: Employee 도메인 정의

---
+++ src/main/java/com/team4/prompt/employee/model/Employee.java
@@ -0,0 +1,61 @@
+package com.team4.prompt.employee.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String userId;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false, unique = true)
+    private String registerNumber;
+    @Column(nullable = false)
+    private String phoneNumber;
+    @Column(nullable = false)
+    private String education;
+    @Column(nullable = false)
+    private int experienceYear;
+    @Column(nullable = false)
+    private LocalDateTime enteringDate;
+    private String skill;
+    private String position;
+    @Column(nullable = false)
+    private boolean approved;
+    @Column(nullable = false)
+    private boolean deleted;
+
+    @Builder
+    private Employee(String userId, String email, String password, String name, String registerNumber, String phoneNumber,
+                     String education, int experienceYear, LocalDateTime enteringDate, String skill, String position) {
+        this.userId = userId;
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.registerNumber = registerNumber;
+        this.phoneNumber = phoneNumber;
+        this.education = education;
+        this.experienceYear = experienceYear;
+        this.enteringDate = enteringDate;
+        this.skill = skill;
+        this.position = position;
+    }
+}
