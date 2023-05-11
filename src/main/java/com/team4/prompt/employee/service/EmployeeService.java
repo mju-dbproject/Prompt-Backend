038768fbcea1bc33a3fca5b0c8d307675bdc21f5
stophwan <jjw981210@gmail.com>
2023-05-12 04:12:16 +0900

    feat: JwtProvider 구현

--- src/main/java/com/team4/prompt/employee/service/EmployeeService.java
+++ src/main/java/com/team4/prompt/employee/service/EmployeeService.java
@@ -1,18 +1,21 @@
 package com.team4.prompt.employee.service;
 
-import com.team4.prompt.employee.controller.dto.EmployeeCreateRequest;
+import com.team4.prompt.employee.model.Employee;
 import com.team4.prompt.employee.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class EmployeeService {
     private final EmployeeRepository employeeRepository;
-    private final PasswordEncoder passwordEncoder;
 
-    public void join(EmployeeCreateRequest employeeCreateRequest) {
-        employeeRepository.save(employeeCreateRequest.newUser(passwordEncoder));
+    public void join(Employee employee) {
+        employeeRepository.save(employee);
+    }
+
+    public Employee findByUserId(String userId) {
+        return employeeRepository.findByUserId(userId)
+                .orElseThrow(()-> new IllegalArgumentException(""));
     }
 }
