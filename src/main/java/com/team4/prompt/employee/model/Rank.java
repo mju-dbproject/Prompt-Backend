f5ddd25f6778802d8ff88af7b95f828a6283fc48
stophwan <jjw981210@gmail.com>
2023-05-12 14:20:45 +0900

    fix: Position 도메인 수정 및 Rank 생성

---
+++ src/main/java/com/team4/prompt/employee/model/Rank.java
@@ -0,0 +1,31 @@
+package com.team4.prompt.employee.model;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import java.util.Arrays;
+import java.util.Objects;
+import lombok.Getter;
+
+@Getter
+public enum Rank {
+    DIRECTOR("임원", 0),
+    SENIOR_MANAGER("수석", 1),
+    MANAGER("책임", 2),
+    SENIOR_ASSISTANCE("선임", 3),
+    ASSISTANCE("사원", 4);
+
+    private final String rankName;
+    private final int rankNumber;
+    Rank(String rankName, int rankNumber) {
+        this.rankName = rankName;
+        this.rankNumber = rankNumber;
+    }
+
+    @JsonCreator
+    public static Rank of(String rankName) {
+        return Arrays.stream(Rank.values())
+                .filter(position -> Objects.nonNull(rankName))
+                .filter(position -> Objects.equals(position.getRankName(), rankName))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(""));
+    }
+}
