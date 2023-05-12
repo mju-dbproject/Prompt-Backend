72222be65d934058eeec4e9eef6529af0f6a643a
stophwan <jjw981210@gmail.com>
2023-05-12 14:47:47 +0900

    feat: 입사 번호 생성 기능 구현

--- src/main/java/com/team4/prompt/employee/model/Employee.java
+++ src/main/java/com/team4/prompt/employee/model/Employee.java
@@ -31,6 +31,9 @@ public class Employee {
     @Column(nullable = false)
     private String password;
 
+    @Column(nullable = false, unique = true)
+    private String employeeNumber;
+
     @Column(nullable = false)
     private String name;
 
@@ -55,6 +58,10 @@ public class Employee {
     @Enumerated(EnumType.STRING)
     private Position position;
 
+    @Column(name = "c_rank", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Rank rank;
+
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private Role role;
@@ -67,7 +74,7 @@ public class Employee {
 
     @Builder
     private Employee(String userId, String email, String password, String name, String registerNumber, String phoneNumber,
-                     String education, int experienceYear, LocalDateTime enteringDate, String skill, Position position, Role role) {
+                     String education, int experienceYear, LocalDateTime enteringDate, String skill, Position position, Rank rank, Role role) {
         this.userId = userId;
         this.email = email;
         this.password = password;
@@ -79,6 +86,26 @@ public class Employee {
         this.enteringDate = enteringDate;
         this.skill = skill;
         this.position = position;
+        this.rank = rank;
         this.role = role;
+    }
+
+    public void giveEmployeeNumber(int enteringOrder) {
+        String number = String.valueOf(enteringDate.getYear() % 100)
+                + rank.getRankNumber()
+                + position.getPositionNumber()
+                + convertEnteringOrder(enteringOrder);
+        if(number.length()!=6) {
+            throw new IllegalArgumentException("");
+        }
+        this.employeeNumber = number;
+    }
+
+    private String convertEnteringOrder(int enteringOrder) {
+        enteringOrder++;
+        if(enteringOrder<10){
+            return "0"+enteringOrder;
+        }
+        return String.valueOf(enteringOrder);
     }
 }
