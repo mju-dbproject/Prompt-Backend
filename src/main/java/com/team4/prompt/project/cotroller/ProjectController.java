a69583eb189d442abaef71140d5d773a177732cc
stophwan <jjw981210@gmail.com>
2023-05-20 03:04:43 +0900

    feat: 권한별 접근 권한 추가

--- src/main/java/com/team4/prompt/project/cotroller/ProjectController.java
+++ src/main/java/com/team4/prompt/project/cotroller/ProjectController.java
@@ -5,6 +5,7 @@ import com.team4.prompt.project.cotroller.dto.ProjectListDto;
 import com.team4.prompt.project.service.ProjectService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,16 +20,19 @@ public class ProjectController {
     private final ProjectService projectService;
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public void createProject(@RequestBody @Valid ProjectCreateRequest projectCreateRequest) {
         projectService.createProject(projectCreateRequest);
     }
 
     @GetMapping("/all")
+    @PreAuthorize("hasRole('ADMIN')")
     public ProjectListDto getAllProject(){
         return projectService.getAllProject();
     }
 
     @GetMapping("/in-progress")
+    @PreAuthorize("hasRole('ADMIN')")
     public ProjectListDto getInProgressProject() {
         return projectService.getInProgressProject();
     }
