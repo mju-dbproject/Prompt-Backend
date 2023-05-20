b609d4d4dbd577b7fb01678a750e57bc7d4d1b75
stophwan <jjw981210@gmail.com>
2023-05-20 13:58:38 +0900

    feat: 직원이 투입된 프로젝트 조회 기능 구현

--- src/main/java/com/team4/prompt/project/service/ProjectService.java
+++ src/main/java/com/team4/prompt/project/service/ProjectService.java
@@ -1,11 +1,14 @@
 package com.team4.prompt.project.service;
 
+import com.team4.prompt.manpower.domain.ManPower;
+import com.team4.prompt.manpower.repository.ManpowerRepository;
 import com.team4.prompt.project.cotroller.dto.ProjectCreateRequest;
 import com.team4.prompt.project.cotroller.dto.ProjectDto;
 import com.team4.prompt.project.cotroller.dto.ProjectListDto;
 import com.team4.prompt.project.domain.Project;
 import com.team4.prompt.project.domain.ProjectStatus;
 import com.team4.prompt.project.repository.ProjectRepository;
+import com.team4.prompt.user.model.User;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class ProjectService {
     private final ProjectRepository projectRepository;
+    private final ManpowerRepository manpowerRepository;
 
     @Transactional
     public void createProject(ProjectCreateRequest projectCreateRequest) {
@@ -51,6 +55,28 @@ public class ProjectService {
     public ProjectListDto getDoneProject() {
         List<Project> projectList = projectRepository.findByStatus(ProjectStatus.FINISH);
         List<ProjectDto> projectDtoList = projectList.stream().map(ProjectDto::new).toList();
+        return new ProjectListDto(projectDtoList);
+    }
+
+    public ProjectListDto getAllProjectForEmployee(User user) {
+        List<ManPower> manpowerList = manpowerRepository.findByUser(user);
+        List<ProjectDto> projectDtoList = manpowerList.stream().map(ManPower::getProject).map(ProjectDto::new).toList();
+        return new ProjectListDto(projectDtoList);
+    }
+
+    public ProjectListDto getInProgressProjectForEmployee(User user) {
+        List<ManPower> manpowerList = manpowerRepository.findByUser(user);
+        List<ProjectDto> projectDtoList = manpowerList.stream().map(ManPower::getProject)
+                .filter(project -> project.getStatus().equals(ProjectStatus.PROGRESS))
+                .map(ProjectDto::new).toList();
+        return new ProjectListDto(projectDtoList);
+    }
+
+    public ProjectListDto getDoneProjectForEmployee(User user) {
+        List<ManPower> manpowerList = manpowerRepository.findByUser(user);
+        List<ProjectDto> projectDtoList = manpowerList.stream().map(ManPower::getProject)
+                .filter(project -> project.getStatus().equals(ProjectStatus.FINISH))
+                .map(ProjectDto::new).toList();
         return new ProjectListDto(projectDtoList);
     }
 }
