b1c21f1930166b95c4d1b6f0e801ccd9a2722c13
stophwan <jjw981210@gmail.com>
2023-05-20 13:34:14 +0900

    fix: 엔티티 세부사항 변경에 따른 적재 스크립트 수정

--- src/main/resources/sql/employee_data.sql
+++ src/main/resources/sql/employee_data.sql
@@ -1,29 +1,29 @@
-INSERT INTO employee(id, user_id, password, employee_no, name, email,registration_number, phone_number, education,
-                     experience_year, entering_date, approved, skill, c_rank, position, role)
-VALUES ( null, 'admin01', 'test', '151001', '관리자1','admin001@gmail.com', '12345678-00000001', '010-0000-0001', '명지대학교 졸업', 10,
-        '2016-03-01 13:10:00', true, null, 'DIRECTOR', null, 'ADMIN');
+INSERT INTO employee(id, user_id, password, employee_number, name, email,register_number, phone_number, education,
+                     experience_year, entering_date, approved, skill, c_rank, position, role, deleted)
+VALUES ( null, 'admin01', '$2a$10$k/JAofWoHWEP1Xz6pW9rseoAOltZRJ4lZga6/aD/REISTuCaZwilS', '151001', '관리자1','admin001@gmail.com', '12345678-00000001', '010-0000-0001', '명지대학교 졸업', 10,
+        '2016-03-01 13:10:00', true, null, 'DIRECTOR', 'DIRECTOR', 'ADMIN', false);
 
-INSERT INTO employee(id, user_id, password, employee_no, name, email,registration_number, phone_number, education,
-                     experience_year, entering_date, approved,skill, c_rank, position, role)
-VALUES ( null, 'admin02', 'test', '151002', '관리자2','admin002@gmail.com', '12345678-00000002', '010-0000-0002', '명지대학교 졸업', 10,
-         '2016-03-01 13:10:00', true, null, 'DIRECTOR', null, 'ADMIN');
+INSERT INTO employee(id, user_id, password, employee_number, name, email,register_number, phone_number, education,
+                     experience_year, entering_date, approved,skill, c_rank, position, role, deleted)
+VALUES ( null, 'admin02', '$2a$10$k/JAofWoHWEP1Xz6pW9rseoAOltZRJ4lZga6/aD/REISTuCaZwilS', '151002', '관리자2','admin002@gmail.com', '12345678-00000002', '010-0000-0002', '명지대학교 졸업', 10,
+         '2016-03-01 13:10:00', true, null, 'DIRECTOR', 'DIRECTOR', 'ADMIN', false);
 
-INSERT INTO employee(id, user_id, password, employee_no, name, email,registration_number, phone_number, education,
-                     experience_year, entering_date, approved,skill, c_rank, position, role)
-VALUES ( null, 'test001', 'test', '171101', '직원1','tester001@gmail.com', '12345678-00000011', '010-0000-0011', '명지대학교 졸업', 10,
-         '2017-03-01 13:10:00', true, 'java spring jpa mysql docker aws', 'SENIOR_MANAGER', 'DEVELOPER', 'USER');
+INSERT INTO employee(id, user_id, password, employee_number, name, email,register_number, phone_number, education,
+                     experience_year, entering_date, approved,skill, c_rank, position, role, deleted)
+VALUES ( null, 'test001', '$2a$10$k/JAofWoHWEP1Xz6pW9rseoAOltZRJ4lZga6/aD/REISTuCaZwilS', '171101', '직원1','tester001@gmail.com', '12345678-00000011', '010-0000-0011', '명지대학교 졸업', 10,
+         '2017-03-01 13:10:00', true, 'java spring jpa mysql docker aws', 'SENIOR_MANAGER', 'DEVELOPER', 'USER', false);
 
-INSERT INTO employee(id, user_id, password, employee_no, name, email,registration_number, phone_number, education,
-                     experience_year, entering_date, approved,skill, c_rank, position, role)
-VALUES ( null, 'test002', 'test', '191001', '직원2','tester002@gmail.com', '12345678-00000012', '010-0000-0012', '명지대학교 졸업', 10,
-         '2020-03-01 13:10:00', true, null, 'SENIOR_ASSISTANCE', 'ADMINISTRATION', 'USER');
+INSERT INTO employee(id, user_id, password, employee_number, name, email,register_number, phone_number, education,
+                     experience_year, entering_date, approved,skill, c_rank, position, role, deleted)
+VALUES ( null, 'test002', '$2a$10$k/JAofWoHWEP1Xz6pW9rseoAOltZRJ4lZga6/aD/REISTuCaZwilS', '191001', '직원2','tester002@gmail.com', '12345678-00000012', '010-0000-0012', '명지대학교 졸업', 10,
+         '2020-03-01 13:10:00', true, null, 'SENIOR_ASSISTANCE', 'ADMINISTRATION', 'USER', false);
 
-INSERT INTO employee(id, user_id, password, employee_no, name, email,registration_number, phone_number, education,
-                     experience_year, entering_date, approved,skill, c_rank, position, role)
-VALUES ( null, 'test003', 'test', '191101', '직원3','tester003@gmail.com', '12345678-00000013', '010-0000-0013', '명지대학교 졸업', 10,
-         '2019-03-01 13:10:00', true, 'java spring jpa mysql docker aws', 'ASSISTANCE', 'DEVELOPER', 'USER');
+INSERT INTO employee(id, user_id, password, employee_number, name, email,register_number, phone_number, education,
+                     experience_year, entering_date, approved,skill, c_rank, position, role, deleted)
+VALUES ( null, 'test003', '$2a$10$k/JAofWoHWEP1Xz6pW9rseoAOltZRJ4lZga6/aD/REISTuCaZwilS', '191101', '직원3','tester003@gmail.com', '12345678-00000013', '010-0000-0013', '명지대학교 졸업', 10,
+         '2019-03-01 13:10:00', true, 'java spring jpa mysql docker aws', 'ASSISTANCE', 'DEVELOPER', 'USER', false);
 
-INSERT INTO employee(id, user_id, password, employee_no, name, email,registration_number, phone_number, education,
-                     experience_year, entering_date, approved,skill, c_rank, position, role)
-VALUES ( null, 'test004', 'test', '191102', '직원4','tester004@gmail.com', '12345678-00000014', '010-0000-0014', '명지대학교 졸업', 10,
-         '2019-03-01 13:10:00', true, 'java spring jpa mysql docker aws', 'ASSISTANCE', 'DEVELOPER', 'USER');
+INSERT INTO employee(id, user_id, password, employee_number, name, email,register_number, phone_number, education,
+                     experience_year, entering_date, approved,skill, c_rank, position, role, deleted)
+VALUES ( null, 'test004', '$2a$10$k/JAofWoHWEP1Xz6pW9rseoAOltZRJ4lZga6/aD/REISTuCaZwilS', '191102', '직원4','tester004@gmail.com', '12345678-00000014', '010-0000-0014', '명지대학교 졸업', 10,
+         '2019-03-01 13:10:00', true, 'java spring jpa mysql docker aws', 'ASSISTANCE', 'DEVELOPER', 'USER', false);
