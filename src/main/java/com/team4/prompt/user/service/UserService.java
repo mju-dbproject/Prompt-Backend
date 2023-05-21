d6f0f9a58eb284e1aa53bf51c19382da20695365
jisu <98259433+jisu-jeong0@users.noreply.github.com>
2023-05-22 03:11:07 +0900

    feat: 회원 정보 수정 기능 구현

--- src/main/java/com/team4/prompt/user/service/UserService.java
+++ src/main/java/com/team4/prompt/user/service/UserService.java
@@ -1,6 +1,7 @@
 package com.team4.prompt.user.service;
 
 import com.team4.prompt.user.controller.dto.UserInfoDto;
+import com.team4.prompt.user.controller.dto.UserUpdateDto;
 import com.team4.prompt.user.model.User;
 import com.team4.prompt.user.repository.UserRepository;
 import java.time.LocalDateTime;
@@ -31,6 +32,18 @@ public class UserService {
 
     public boolean checkId(String userId) {
         return userRepository.findByUserId(userId).isEmpty();
+    }
+
+    public void updateUserInfo(String employeeNumber, UserUpdateDto userUpdateDto) {
+        User user = userRepository.findByEmployeeNumber(employeeNumber)
+                .orElseThrow(()-> new IllegalArgumentException("회원이 존재하지 않습니다."));
+
+        userUpdateDto.name().ifPresent(user::updateName);
+        userUpdateDto.email().ifPresent(user::updateEmail);
+        userUpdateDto.phoneNumber().ifPresent(user::updatePhoneNumber);
+        userUpdateDto.skill().ifPresent(user::updateSkill);
+
+        userRepository.save(user);
     }
 
     public UserInfoDto getMyInfo(String employeeNumber) {
