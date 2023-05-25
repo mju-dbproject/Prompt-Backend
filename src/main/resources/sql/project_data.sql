f08ce794f64cc467a95f791581ee58c1b85cb928
tk.yoon <tk.yoon0105@gmail.com>
2023-05-26 01:45:03 +0900

    feat: 데이터 적재 스크립트 추가(프로젝트)

--- src/main/resources/sql/project_data.sql
+++ src/main/resources/sql/project_data.sql
@@ -2,10 +2,19 @@ INSERT INTO project(id, project_number, name, create_date, start_date, end_date,
 VALUES (null, '170101', '명지대 홈페이지', '2017-01-01 13:10:00', '2017-03-01 13:10:00', '2018-01-01 13:10:00', '명지대학교', 3000000, 'FINISH', '명지대학교 홈페이지 개발', false);
 
 INSERT INTO project(id, project_number, name, create_date, start_date, end_date, client, budget, status, description, deleted)
+VALUES (null, '180801', '외교부 홈페이지', '2018-08-11 10:00:00', '2018-09-01 10:00:00', '2019-09-01 10:00:00', '외교부', 10000000, 'FINISH', '외교부 홈페이지 개발', false);
+
+INSERT INTO project(id, project_number, name, create_date, start_date, end_date, client, budget, status, description, deleted)
 VALUES (null, '190401', '반려생활', '2019-04-18 13:10:00', null, null, '마티즈', 1000000, 'READY', '애견 동반 코스 소개 앱', false);
 
 INSERT INTO project(id, project_number, name, create_date, start_date, end_date, client, budget, status, description, deleted)
 VALUES (null, '191202', '공공마스크 웹사이트', '2019-12-18 13:10:00', '2019-12-20 13:10:00', '2020-01-10 13:10:00', '질병관리청', 1000000, 'FINISH', '공공마스크 재고 관리 확인 사이트', false);
+
+INSERT INTO project(id, project_number, name, create_date, start_date, end_date, client, budget, status, description, deleted)
+VALUES (null, '200501', '통일 지킴 플랫폼 1차 구축사업', '2020-05-30 12:00:00', '2022-11-01 12:00:00', '2023-11-01 12:00:00', '통일부', 20000000, 'FINISH', '통일 지킴 플랫폼 웹사이트, 앱 개발', false);
+
+INSERT INTO project(id, project_number, name, create_date, start_date, end_date, client, budget, status, description, deleted)
+VALUES (null, '210501', '통일 지킴 플랫폼 2차 구축사업', '2021-05-30 12:00:00', '2022-11-01 12:00:00', null, '통일부', 300000000, 'PROGRESS', '통일 지킴 플랫폼 웹사이트, 앱 개발 고도화 작업', false);
 
 INSERT INTO project(id, project_number, name, create_date, start_date, end_date, client, budget, status, description, deleted)
 VALUES (null, '220501', '투썸플레이스 앱 기능 개발', '2022-05-30 12:00:00', '2022-10-31 12:00:00', null, '투썸플레이스', 2000000, 'PROGRESS', '투썸플레이스 케이크 주문제작 기능 추가', false);
