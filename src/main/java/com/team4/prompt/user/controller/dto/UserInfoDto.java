9f65959521f5f436cd02ac16bb6a27cfef4ab2e1
jisu <98259433+jisu-jeong0@users.noreply.github.com>
2023-05-28 19:42:01 +0900

    feat: 회원 정보 조회 시 rank, positon 추가

--- src/main/java/com/team4/prompt/user/controller/dto/UserInfoDto.java
+++ src/main/java/com/team4/prompt/user/controller/dto/UserInfoDto.java
@@ -1,5 +1,7 @@
 package com.team4.prompt.user.controller.dto;
 
+import com.team4.prompt.user.model.Position;
+import com.team4.prompt.user.model.Rank;
 import com.team4.prompt.user.model.User;
 import lombok.Builder;
 import lombok.Data;
@@ -11,6 +13,8 @@ public class UserInfoDto {
     private final String phoneNumber;
     private final String email;
     private final String skill;
+    private final Rank rank;
+    private final Position position;
 
     @Builder
     public UserInfoDto(User user){
@@ -18,5 +22,7 @@ public class UserInfoDto {
         this.phoneNumber = user.getPhoneNumber();
         this.email = user.getEmail();
         this.skill = user.getSkill();
+        this.rank = user.getRank();
+        this.position = user.getPosition();
     }
 }
