b587f361e4b7071c6eb0fb08e873939526552281
jisu <98259433+jisu-jeong0@users.noreply.github.com>
2023-05-30 15:03:41 +0900

    fix: 아이디 찾기 응답 형식 수정

---
+++ src/main/java/com/team4/prompt/user/controller/dto/FindUserIdResponse.java
@@ -0,0 +1,10 @@
+package com.team4.prompt.user.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class FindUserIdResponse {
+    private String userId;
+}
